4d0b2625d004846e2b8a3132bcc60e604cdc15f7
gabrielporcher <gabrielporcher95@gmail.com>
2025-10-21 16:47:33 -0300

    Estrutura basica dados mockados

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function SignInScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: "white" }}>Signin screen</Text>
+      <Text>Signin screen</Text>
     </View>
   );
 }
